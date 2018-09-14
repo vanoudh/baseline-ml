b2dd10f144370b9e096ec2d4c9befc039885f390
vanoudh <marc.vanoudheusden@gmail.com>
2018-09-14 11:38:25 -0700

    coded gcp storage

--- storage_gcp.py
+++ storage_gcp.py
@@ -7,10 +7,15 @@ project_id = os.getenv('GOOGLE_CLOUD_PROJECT')
 dc = datastore.Client(project_id)
 sc = storage.Client(project_id)
 
-STORAGE_FOLDER = 'gs://{}-media'.format(project_id)
+STORAGE_FOLDER = '{}-media'.format(project_id)
 S = '-'
 
+print(STORAGE_FOLDER)
 bucket = sc.get_bucket(STORAGE_FOLDER)
+
+blob = bucket.blob('testblob')
+blob.upload_from_string('content of test blob\n')
+blob.download_to_filename('testblob.txt')
 
 
 class FileStore:
@@ -18,15 +23,21 @@ class FileStore:
 
     def save(self, user_id, file_id, file):
         """Doc."""
-        path = self.get_path(user_id, file_id)
+        path = self._get_path(user_id, file_id)
         blob = bucket.blob(path)
         blob.upload_from_string(file.read(), content_type=file.content_type)
-        # file.save(path)
 
-    def get_path(self, user_id, file_id):
+    def _get_path(self, user_id, file_id):
         """Doc."""
         li = [str(user_id), str(file_id)]
         return S.join(li)
+
+    def get_path(self, user_id, file_id):
+        """Doc."""
+        path = self._get_path(user_id, file_id)
+        blob = bucket.blob(path)
+		blob.download_to_filename(path)
+        return path
 
 
 class DocStore:
