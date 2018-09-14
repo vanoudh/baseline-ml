10b388d4da03b70f28e2c36eaa76bd60e7b68e84
vanoudh <marc.vanoudheusden@gmail.com>
2018-09-14 11:09:48 -0700

    blob storage

--- storage_gcp.py
+++ storage_gcp.py
@@ -1,16 +1,16 @@
 """Doc."""
 import os
-from google.cloud import datastore
+from google.cloud import datastore, storage
 
 
 project_id = os.getenv('GOOGLE_CLOUD_PROJECT')
-client = datastore.Client(project_id)
+dc = datastore.Client(project_id)
+sc = storage.Client(project_id)
 
-STORAGE_FOLDER = 'store'
+STORAGE_FOLDER = 'gs://{}-media'.format(project_id)
 S = '-'
 
-if not os.path.isdir(STORAGE_FOLDER):
-    os.mkdir(STORAGE_FOLDER)
+bucket = sc.get_bucket(STORAGE_FOLDER)
 
 
 class FileStore:
@@ -19,30 +19,32 @@ class FileStore:
     def save(self, user_id, file_id, file):
         """Doc."""
         path = self.get_path(user_id, file_id)
-        file.save(path)
+        blob = bucket.blob(path)
+        blob.upload_from_string(file.read(), content_type=file.content_type)
+        # file.save(path)
 
     def get_path(self, user_id, file_id):
         """Doc."""
         li = [str(user_id), str(file_id)]
-        return os.path.join(STORAGE_FOLDER, S.join(li))
+        return S.join(li)
 
 
 class DocStore:
     """Doc."""
 
     def _key(self, user_id, doc_id):
-        return client.key(str(user_id), doc_id)
+        return dc.key(str(user_id), doc_id)
 
     def put(self, user_id, doc_id, doc):
         """Doc."""
         print('put', doc)
         entity = datastore.Entity(self._key(user_id, doc_id))        
         entity.update(doc)
-        client.put(entity)
+        dc.put(entity)
 
     def get(self, user_id, doc_id):
         """Doc."""
-        r = client.get(self._key(user_id, doc_id))
+        r = dc.get(self._key(user_id, doc_id))
         if r is None:
             return r
         doc = {}
@@ -53,5 +55,5 @@ class DocStore:
 
     def delete(self, user_id, doc_id):
         """Doc."""
-        return client.delete(self._key(user_id, doc_id))
+        return dc.delete(self._key(user_id, doc_id))
 
--- test_storage.py
+++ test_storage.py
@@ -9,29 +9,42 @@ id = secrets.token_hex(8)
 key = secrets.token_hex(8)
 
 
+js = {
+    "PassengerId": 1, 
+    "Survived": "target", 
+    "Pclass": "predictor", 
+    "Sex": "predictor", 
+    "Fare": "predictor"
+    }
+
 def test1():
     """Put, get, delete."""
-    ds.put(id, key, {'test': 42})
-    r = ds.get(id, key)
-    assert r['test'] == 42
-    ds.delete(id, key)
-    assert ds.get(id, key) == None
-
-js = {
-    "PassengerId": "predictor", "Survived": "target", 
-    "Pclass": "predictor", "Sex": "predictor", 
-    "Age": "predictor", "Fare": "predictor"}
-
-def test2():
-    """Put then get."""
     ds.put(id, key, js)
     r = ds.get(id, key)
-    assert r['PassengerId'] == 'predictor'
+    assert r['PassengerId'] == 1
     assert r['Survived'] == 'target'
     assert r == js
     print(r)
     ds.delete(id, key)
     assert ds.get(id, key) == None
 
-# test2()
+
+js = {
+    "PassengerId": 1, 
+    "Survived": "target", 
+    "Pclass": {'a': 3}, 
+    "Sex": "predictor", 
+    "Fare": "predictor"
+    }
+
+def test2():
+    """Put, get, delete."""
+    ds.put(id, key, js)
+    r = ds.get(id, key)
+    assert r['Pclass']['a'] == 3
+    assert r == js
+    print(r)
+    ds.delete(id, key)
+    assert ds.get(id, key) == None
+
 
