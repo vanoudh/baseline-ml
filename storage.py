d342cb442f7cc65fd1620bbaabb5e1715c3a51b5
vanoudh <marc.vanoudheusden@gmail.com>
2018-09-14 12:25:18 -0700

    synch storage

--- storage.py
+++ storage.py
@@ -1,16 +1,24 @@
 """Doc."""
 import os
-from google.cloud import datastore
+from google.cloud import datastore, storage
 
-
-project_id = os.getenv('GOOGLE_CLOUD_PROJECT')
-client = datastore.Client(project_id)
-
-STORAGE_FOLDER = 'store'
+STORAGE_FOLDER = 'local_storage'
 S = '-'
 
 if not os.path.isdir(STORAGE_FOLDER):
     os.mkdir(STORAGE_FOLDER)
+
+project_id = os.getenv('GOOGLE_CLOUD_PROJECT')
+dc = datastore.Client(project_id)
+sc = storage.Client(project_id)
+
+BUCKET = '{}-media'.format(project_id)
+
+bucket = sc.get_bucket(BUCKET)
+
+# blob = bucket.blob('testblob')
+# blob.upload_from_string('content of test blob\n')
+# blob.download_to_filename('testblob.txt')
 
 
 class FileStore:
@@ -18,31 +26,40 @@ class FileStore:
 
     def save(self, user_id, file_id, file):
         """Doc."""
-        path = self.get_path(user_id, file_id)
-        file.save(path)
+        filename = self._get_filename(user_id, file_id)
+        blob = bucket.blob(filename)
+        blob.upload_from_string(file.read(), content_type=file.content_type)
+
+    def _get_filename(self, user_id, file_id):
+        """Doc."""
+        li = [str(user_id), str(file_id)]
+        return S.join(li)
 
     def get_path(self, user_id, file_id):
         """Doc."""
-        li = [str(user_id), str(file_id)]
-        return os.path.join(STORAGE_FOLDER, S.join(li))
+        filename = self._get_filename(user_id, file_id)
+        blob = bucket.blob(filename)
+        path = os.path.join(STORAGE_FOLDER, filename)
+        blob.download_to_filename(path)
+        return path
 
 
 class DocStore:
     """Doc."""
 
     def _key(self, user_id, doc_id):
-        return client.key(str(user_id), doc_id)
+        return dc.key(str(user_id), doc_id)
 
     def put(self, user_id, doc_id, doc):
         """Doc."""
         print('put', doc)
         entity = datastore.Entity(self._key(user_id, doc_id))        
         entity.update(doc)
-        client.put(entity)
+        dc.put(entity)
 
     def get(self, user_id, doc_id):
         """Doc."""
-        r = client.get(self._key(user_id, doc_id))
+        r = dc.get(self._key(user_id, doc_id))
         if r is None:
             return r
         doc = {}
@@ -53,5 +70,5 @@ class DocStore:
 
     def delete(self, user_id, doc_id):
         """Doc."""
-        return client.delete(self._key(user_id, doc_id))
+        return dc.delete(self._key(user_id, doc_id))
 
